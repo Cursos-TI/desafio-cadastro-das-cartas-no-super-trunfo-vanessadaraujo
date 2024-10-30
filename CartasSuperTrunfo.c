ed0d27534abf333ef2a48260cddda33cee7ee649
vanessadaraujo <vanessa.sdearaujo@gmail.com>
2024-10-30 22:11:07 +0000

    iniciando o código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,6 +16,34 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    printf("SuperTrunfo países \n");
+    //Variáveis a serem trabalhadas
+
+    int codigo, populacao, pontosturisticos;
+    char nome[50];
+    float area;
+    double pib;
+
+    //Início do cadastro das cartas
+    printf("Insira o nome da cidade:\n");
+    scanf ("%s", &nome);
+    
+    printf("Cadastre um código para sua carta: \n");
+    scanf("%d", &codigo);
+
+    printf("Informe a população: \n");
+    scanf(" %d", &populacao);
+
+    printf("Qual a área? \n");
+    scanf("%f", &area);
+
+    printf("Qual o PIB? \n");
+    scanf("%f", &pib);
+
+    printf("Informe o número de pontos turísticos: \n");
+    scanf("%d", &pontosturisticos);
+
 
     return 0;
 }
