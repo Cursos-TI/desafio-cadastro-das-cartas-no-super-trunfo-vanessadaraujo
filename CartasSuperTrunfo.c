d79702fbee45e4c456dedce12197382db0c0a0f0
vanessadaraujo <vanessa.sdearaujo@gmail.com>
2024-10-31 19:53:03 +0000

    novas atualizações no codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,33 +17,50 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     
-    printf("SuperTrunfo países \n");
+    printf("SuperTrunfo países \n \n");
     //Variáveis a serem trabalhadas
 
-    int codigo, populacao, pontosturisticos;
-    char nome[50];
-    float area;
-    double pib;
+    int populacao, pontosturisticos;
+    char pais[50], estado[50], cidade[50], codigo[50];
+    float area, pib;
 
     //Início do cadastro das cartas
-    printf("Insira o nome da cidade:\n");
-    scanf ("%s", &nome);
+
+    printf("Vamos cadastrar o país! \n");    
+    printf("Insira o nome do país: \n");
+    scanf("%s", &pais);
+
+    printf("Agora, vamos cadastrar o estado! \n");    
+    printf("Insira o nome do estado: \n");
+    scanf("%s", &estado);
+    
+    printf("Agora chegou o momento de cadastrar informações sobre a cidade! \n");
+    printf("Informe o nome da cidade: \n");
+    scanf ("%s", &cidade);
     
     printf("Cadastre um código para sua carta: \n");
-    scanf("%d", &codigo);
+    scanf("%s", &codigo);
 
-    printf("Informe a população: \n");
-    scanf(" %d", &populacao);
+    printf("Informe a população dessa cidade: \n");
+    scanf("%d", &populacao);
 
-    printf("Qual a área? \n");
-    scanf("%f", &area);
+    printf("Qual a área territorial dessa cidade? \n");
+    scanf(" %f", &area);
 
-    printf("Qual o PIB? \n");
+    printf("Qual o PIB dessa cidade? \n");
     scanf("%f", &pib);
 
-    printf("Informe o número de pontos turísticos: \n");
+    printf("Informe o número de pontos turísticos dessa cidade: \n");
     scanf("%d", &pontosturisticos);
 
+//Agora, a forma como esses dados serão apresentados:
+
+    printf("País: %s - Estado: %s - Cidade: %s\n", pais, estado, cidade);
+    printf("Código da carta %s\n", codigo);
+    printf("A população dessa cidade é de %d\n",populacao);
+    printf("A área territorial dessa cidade é de %f\n",area);
+    printf("O PIB dessa cidade é %f\n",pib);
+    printf("Essa cidade tem %d pontos turísticos.\n", pontosturisticos);
 
     return 0;
 }
